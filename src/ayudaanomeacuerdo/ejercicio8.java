1150e8951d7f8532728f73a4a8c6d620080b29c7
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-04-14 20:56:34 -0500

    second commit

--- src/ayudaanomeacuerdo/ejercicio8.java
+++ src/ayudaanomeacuerdo/ejercicio8.java
@@ -4,18 +4,18 @@ public class ejercicio8 {
 
     public static void main(String[] args) {
         Random ale=new Random();
-        Scanner t= new Scanner(System.in);
+        Scanner tec= new Scanner(System.in);
         int vec []=new int[100];
         for (int i = 0; i < 100; i++) {
             vec[i]=ale.nextInt(100);
         }
         System.out.println("los valores ramdon fueros:");
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 90; i++) {
             System.out.print(vec[i]+" ");
         }
         System.out.println();
         System.out.println("escriba un numero:");
-        int numero =t.nextInt();
+        int numero =tec.nextInt();
         int cont=0;
         for (int i = 0; i < 100; i++) {
             if(vec[i]==numero){
