2ae60451ebb6c02178846c3fa7567f29048dad9b
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-05-29 20:05:32 -0500

    first commit ejercicios

--- src/primer/trabajo/PrimerTrabajo.java
+++ src/primer/trabajo/PrimerTrabajo.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class PrimerTrabajo {
 
     public static void main(String[] args) {
-        Scanner t= new Scanner(System.in);
-        double b,a;
-        System.out.println("ingrese la base del rectangulo");
-        b=t.nextDouble();
-        System.out.println("ingrese la altura del rectangulo");
-        a=t.nextDouble();
+        Scanner texto= new Scanner(System.in);
         
-        double area = b*a;
-        double peri = 2*(b+a);
+        System.out.println("ingrese la base del rectangulo");
+        double base=texto.nextDouble();
+        System.out.println("ingrese la altura del rectangulo");
+        double altura=texto.nextDouble();
+        
+        double area = base*altura;
+        double perimetro = 2*(base+altura);
         System.out.println("el area es "+area);
-        System.out.println("el perimetro es "+peri);
+        System.out.println("el perimetro es "+perimetro);
     }
     
 }
