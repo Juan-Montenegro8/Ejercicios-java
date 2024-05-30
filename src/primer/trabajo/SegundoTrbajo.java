235302936ab98bb9eaa8b83a6d362e79fe0fe154
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-05-30 18:48:25 -0500

    second commit ejercicio

--- src/primer/trabajo/SegundoTrbajo.java
+++ src/primer/trabajo/SegundoTrbajo.java
@@ -3,20 +3,20 @@ import java.util.*;
 public class SegundoTrbajo {
 
     public static void main(String[] args) {
-        Scanner t=new Scanner(System.in);
+        Scanner texto=new Scanner(System.in);
         
         System.out.println("de la medida en metros");
-        double met=t.nextDouble();
+        double medida=texto.nextDouble();
         
-        double centi= met*100;
-        double pul=centi/2.54;
-        double pie = pul/12;
-        double yar = pie/3;
+        double centimetros= medida*100;
+        double pulgadas=centimetros/2.54;
+        double pie = pulgadas/12;
+        double yardas = pie/3;
         
-        System.out.println("en cm es "+centi);
-        System.out.println("en pulgadas es "+pul);
+        System.out.println("en cm es "+centimetros);
+        System.out.println("en pulgadas es "+pulgadas);
         System.out.println("en pies es "+pie);
-        System.out.println("en yardas es "+yar);        
+        System.out.println("en yardas es "+yardas);      
     }
     
 }
