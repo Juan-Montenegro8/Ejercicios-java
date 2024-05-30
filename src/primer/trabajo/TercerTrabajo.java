d9416afe351fa0c3487e0d12dd1389cfc0525a6b
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-05-30 18:50:28 -0500

    second commit ejercicios

--- src/primer/trabajo/TercerTrabajo.java
+++ src/primer/trabajo/TercerTrabajo.java
@@ -5,27 +5,27 @@ import java.util.*;
 public class TercerTrabajo {
 
     public static void main(String[] args) {
-        Scanner t= new Scanner(System.in);
+        Scanner textoScanner= new Scanner(System.in);
         
         System.out.println("ingrese monto en dolares que donaron");
-        double dol=t.nextDouble();
+        double dolares=textoScanner.nextDouble();
         System.out.println("ingrese monto en euros que donaron");
-        double eur=t.nextDouble();
+        double euros=textoScanner.nextDouble();
         
-        double pes1=dol*2000;
-        double pes2=eur*2800;
+        double peso1=dolares*2000;
+        double peso2=euros*2800;
         
-        double peso=pes1+pes2;
+        double peso=peso1+peso2;
         
         System.out.println("Monto total en peso"+peso);
         
         double salud = peso*0.5;
         double comedor = peso*0.3;
-        double admin = peso*0.2;
+        double administrativos = peso*0.2;
         
         System.out.println("Monto correspondiente a salud "+salud);
         System.out.println("Monto correspondinte al comedor de los niños "+comedor);
-        System.out.println("Monto correspondiente a gastos administrativos "+admin);
+        System.out.println("Monto correspondiente a gastos administrativos "+administrativos);
     }
     
 }
