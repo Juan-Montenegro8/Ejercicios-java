a913698bcf7cc3391e1323509493b64bd7df03df
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-05-31 09:39:02 -0500

    third commit ejercicio

--- src/primer/trabajo/CuartoTrabajo.java
+++ src/primer/trabajo/CuartoTrabajo.java
@@ -3,20 +3,20 @@ import java.util.*;
 public class CuartoTrabajo {
 
     public static void main(String[] args) {
-        Scanner t = new Scanner(System.in);
+        Scanner texto = new Scanner(System.in);
         
         System.out.println("ingrese un numero de 4 cifras");
-        int num=t.nextInt();
+        int numero=texto.nextInt();
                 
-        if((num>=1000) && (num<=9999)){
+        if((numero>=1000) && (numero<=9999)){
             
-            System.out.println("el numero en unidades de mil seria "+num);
-            int num1=num%1000;
-            System.out.println("el numero en centenas seria "+num1);
-            int num2=num1%100;
-            System.out.println("el numero en decenas seria "+num2);
-            int num3=num2%10;
-            System.out.println("el numero en unidades seria "+num3);
+            System.out.println("el numero en unidades de mil seria "+numero);
+            int numemro1=numero%1000;
+            System.out.println("el numero en centenas seria "+numemro1);
+            int nummero2=numemro1%100;
+            System.out.println("el numero en decenas seria "+nummero2);
+            int numero3=nummero2%10;
+            System.out.println("el numero en unidades seria "+numero3);
             
         }else{
             System.out.println("no es numero de cuatro cifras positivo");
