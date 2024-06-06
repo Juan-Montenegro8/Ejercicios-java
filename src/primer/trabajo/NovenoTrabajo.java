117a806f8781a6482ee51f9c639337d9ac08f7eb
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-06-05 20:33:27 -0500

    sixth commit ejercicios

--- src/primer/trabajo/NovenoTrabajo.java
+++ src/primer/trabajo/NovenoTrabajo.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class NovenoTrabajo {
 
     public static void main(String[] args) {
-        Scanner t=new Scanner(System.in);
+        Scanner textoScanner=new Scanner(System.in);
         
-        int j=0;
+        int contenedor=0;
         for(int i=1;i<4;i++){
             System.out.println("ingresa el valor de la venta");
-            int v=t.nextInt();
-            j=j+v;
+            int venta=textoScanner.nextInt();
+            contenedor=contenedor+venta;
         }
         
-        double por=j*0.12;
-        double fin=por+680000;
+        double porcentaje=contenedor*0.12;
+        double sueldo=porcentaje+680000;
         
-        System.out.println("el sueldo del mes fue de: "+fin);
+        System.out.println("el sueldo del mes fue de: "+sueldo);
     }
     
 }
