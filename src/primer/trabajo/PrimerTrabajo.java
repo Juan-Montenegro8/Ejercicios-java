1646a89f2af65b164e15791f09b70f863651fbd0
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-19 20:19:15 -0500

    firts commit scanner

--- src/primer/trabajo/PrimerTrabajo.java
+++ src/primer/trabajo/PrimerTrabajo.java
@@ -3,17 +3,21 @@ import java.util.*;
 public class PrimerTrabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner= new Scanner(System.in);
+        try (Scanner textoScanner= new Scanner(System.in)){
+            
+            System.out.println("ingrese la base del rectangulo");
+            double base=textoScanner.nextDouble();
+            System.out.println("ingrese la altura del rectangulo");
+            double altura=textoScanner.nextDouble();
         
-        System.out.println("ingrese la base del rectangulo");
-        double base=textoScanner.nextDouble();
-        System.out.println("ingrese la altura del rectangulo");
-        double altura=textoScanner.nextDouble();
-        
-        double area = base*altura;
-        double perimetro = 2*(base+altura);
-        System.out.println("el area es "+area);
-        System.out.println("el perimetro es "+perimetro);
+            double area = base*altura;
+            double perimetro = 2*(base+altura);
+            System.out.println("el area es "+area);
+            System.out.println("el perimetro es "+perimetro);
+
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
 }
