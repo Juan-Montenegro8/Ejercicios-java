592bc8e02b09713cfc43f85208bf0a3931bd4b5f
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-20 20:44:47 -0500

    Firt commit scanner

--- src/primer/trabajo/SegundoTrbajo.java
+++ src/primer/trabajo/SegundoTrbajo.java
@@ -3,20 +3,23 @@ import java.util.*;
 public class SegundoTrbajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("de la medida en metros");
-        double medida=textoScanner.nextDouble();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("de la medida en metros");
+            double medida=textoScanner.nextDouble();
         
-        double centimetros= medida*100;
-        double pulgadas=centimetros/2.54;
-        double pie = pulgadas/12;
-        double yardas = pie/3;
+            double centimetros= medida*100;
+            double pulgadas=centimetros/2.54;
+            double pie = pulgadas/12;
+            double yardas = pie/3;
         
-        System.out.println("en cm es "+centimetros);
-        System.out.println("en pulgadas es "+pulgadas);
-        System.out.println("en pies es "+pie);
-        System.out.println("en yardas es "+yardas);      
+            System.out.println("en cm es "+centimetros);
+            System.out.println("en pulgadas es "+pulgadas);
+            System.out.println("en pies es "+pie);
+            System.out.println("en yardas es "+yardas); 
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
 }
