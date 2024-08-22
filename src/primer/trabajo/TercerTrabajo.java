b0a4c5ca43246fb98a73fccceea6fffd8942b3e5
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-21 21:07:58 -0500

    second commit scanner

--- src/primer/trabajo/TercerTrabajo.java
+++ src/primer/trabajo/TercerTrabajo.java
@@ -5,27 +5,30 @@ import java.util.*;
 public class TercerTrabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner= new Scanner(System.in);
         
-        System.out.println("ingrese monto en dolares que donaron");
-        double dolares=textoScanner.nextDouble();
-        System.out.println("ingrese monto en euros que donaron");
-        double euros=textoScanner.nextDouble();
+        try (Scanner textoScanner= new Scanner(System.in)) {
+            System.out.println("ingrese monto en dolares que donaron");
+            double dolares=textoScanner.nextDouble();
+            System.out.println("ingrese monto en euros que donaron");
+            double euros=textoScanner.nextDouble();
         
-        double peso1=dolares*2000;
-        double peso2=euros*2800;
+            double peso1=dolares*2000;
+            double peso2=euros*2800;
         
-        double peso=peso1+peso2;
+            double peso=peso1+peso2;
         
-        System.out.println("Monto total en peso"+peso);
+            System.out.println("Monto total en peso"+peso);
         
-        double salud = peso*0.5;
-        double comedor = peso*0.3;
-        double administrativos = peso*0.2;
+            double salud = peso*0.5;
+            double comedor = peso*0.3;
+            double administrativos = peso*0.2;
         
-        System.out.println("Monto correspondiente a salud "+salud);
-        System.out.println("Monto correspondinte al comedor de los niños "+comedor);
-        System.out.println("Monto correspondiente a gastos administrativos "+administrativos);
+            System.out.println("Monto correspondiente a salud "+salud);
+            System.out.println("Monto correspondinte al comedor de los niños "+comedor);
+            System.out.println("Monto correspondiente a gastos administrativos "+administrativos);    
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
     
 }
