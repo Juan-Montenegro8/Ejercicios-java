763343873c10bc49688443aab58e3ab966ba1fea
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-22 21:05:14 -0500

    Seconds commit scanner

--- src/primer/trabajo/QuientoTrabajo.java
+++ src/primer/trabajo/QuientoTrabajo.java
@@ -5,20 +5,23 @@ import java.util.Scanner;
 public class QuientoTrabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese la cantidad de hombres en el salon");
-        double hombres=textoScanner.nextDouble();
-        System.out.println("ingrese la cantidad de mujeres en el salon");
-        double mujeres=textoScanner.nextDouble();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese la cantidad de hombres en el salon");
+            double hombres=textoScanner.nextDouble();
+            System.out.println("ingrese la cantidad de mujeres en el salon");
+            double mujeres=textoScanner.nextDouble();
         
-        double cantidad=hombres+mujeres;
+            double cantidad=hombres+mujeres;
         
-        double porcentajeHombres=(100*hombres)/cantidad;
-        double porcentajeMujeres=(100*mujeres)/cantidad;
+            double porcentajeHombres=(100*hombres)/cantidad;
+            double porcentajeMujeres=(100*mujeres)/cantidad;
         
-        System.out.println("la cantidad de hombres es del "+porcentajeHombres+"%");
-        System.out.println("la cantidad de mujeres es del "+porcentajeMujeres+"%");
+            System.out.println("la cantidad de hombres es del "+porcentajeHombres+"%");
+            System.out.println("la cantidad de mujeres es del "+porcentajeMujeres+"%");
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error "+e.getMessage());
+        }
     }
     
 }
