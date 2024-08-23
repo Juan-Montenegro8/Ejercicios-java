079718457179463e5f34cd5bb5a14aba57db1ef3
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-22 21:05:49 -0500

    Second commit scanner

--- src/primer/trabajo/SextoTrabajo.java
+++ src/primer/trabajo/SextoTrabajo.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class SextoTrabajo {
 
     public static void main(String[] args) {
-        Scanner texto=new Scanner(System.in);
+        Scanner textoScanner=new Scanner(System.in);
         
         System.out.println("ingrese 1 si tiene grados Fahrenheit o 2 si tiene grados centigrados");
-        int respuesta=texto.nextInt();
+        int respuesta=textoScanner.nextInt();
         
         if(respuesta==1){
             System.out.println("cantidad de grados Fahrenheit");
-            double Fahrenheit = texto.nextDouble();
+            double Fahrenheit = textoScanner.nextDouble();
             double resultado=(Fahrenheit-32)*0.5555555555555556;
             System.out.println("en grados centigrados es "+resultado);
         }else if(respuesta==2){
             
                 System.out.println("cantidad de grados centigrados");
-                double centigrados = texto.nextDouble();
+                double centigrados = textoScanner.nextDouble();
                 double resultado=(centigrados*1.8)+32;
                 System.out.println("en grados centigrados es "+resultado);
             }else{
