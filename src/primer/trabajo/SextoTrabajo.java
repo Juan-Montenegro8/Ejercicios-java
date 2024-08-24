9e89e625e1e40faf61da623730e9cd5e59870703
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-23 20:42:44 -0500

    thirds commit scanner

--- src/primer/trabajo/SextoTrabajo.java
+++ src/primer/trabajo/SextoTrabajo.java
@@ -5,23 +5,26 @@ import java.util.Scanner;
 public class SextoTrabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese 1 si tiene grados Fahrenheit o 2 si tiene grados centigrados");
-        int respuesta=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)){
+            System.out.println("ingrese 1 si tiene grados Fahrenheit o 2 si tiene grados centigrados");
+            int respuesta=textoScanner.nextInt();
         
-        if(respuesta==1){
-            System.out.println("cantidad de grados Fahrenheit");
-            double Fahrenheit = textoScanner.nextDouble();
-            double resultado=(Fahrenheit-32)*0.5555555555555556;
-            System.out.println("en grados centigrados es "+resultado);
-        }else if(respuesta==2){
+            if(respuesta==1){
+                System.out.println("cantidad de grados Fahrenheit");
+                double Fahrenheit = textoScanner.nextDouble();
+                double resultado=(Fahrenheit-32)*0.5555555555555556;
+                System.out.println("en grados centigrados es "+resultado);
+            }else if(respuesta==2){
             
-            System.out.println("cantidad de grados centigrados");
-            double centigrados = textoScanner.nextDouble();
-            double resultado=(centigrados*1.8)+32;
-            System.out.println("en grados centigrados es "+resultado);
-        }else System.out.println("no existe esa opcion");
+                System.out.println("cantidad de grados centigrados");
+                double centigrados = textoScanner.nextDouble();
+                double resultado=(centigrados*1.8)+32;
+                System.out.println("en grados centigrados es "+resultado);
+            }else System.out.println("no existe esa opcion");
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
 }
