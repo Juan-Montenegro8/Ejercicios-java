87ec199a69581f59ad9f92e7c001793697c74ecc
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-26 19:58:03 -0500

    Thirds commit scanner

--- src/primer/trabajo/SeptimoTrabajo.java
+++ src/primer/trabajo/SeptimoTrabajo.java
@@ -5,26 +5,28 @@ import java.util.Scanner;
 public class SeptimoTrabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        int contenedor=0,contenedor0=0;
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            int contenedor=0,contenedor0=0;
         
-        System.out.println("ingrese un valor");
-        int numero=textoScanner.nextInt();
-        System.out.println("ingrese otro");
-        int numero1=textoScanner.nextInt();
+            System.out.println("ingrese un valor");
+            int numero=textoScanner.nextInt();
+            System.out.println("ingrese otro");
+            int numero1=textoScanner.nextInt();
                        
-        contenedor=numero;
-        contenedor0=numero1;
+            contenedor=numero;
+            contenedor0=numero1;
         
-        numero=0;
-        numero1=0;
+            numero=0;
+            numero1=0;
         
-        numero=contenedor0;
-        numero1=contenedor;
+            numero=contenedor0;
+            numero1=contenedor;
         
-        System.out.println("los valores son "+numero+" y "+numero1);
-        
+            System.out.println("los valores son "+numero+" y "+numero1);
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
 }
