5e41542b88d4c612a5c9108079833aaa8792ea58
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-26 19:59:29 -0500

    Third commit scanner

--- src/primer/trabajo/OctavoTabajo.java
+++ src/primer/trabajo/OctavoTabajo.java
@@ -5,18 +5,21 @@ import java.util.Scanner;
 public class OctavoTabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("escriba cuantos GB tiene su disco duro");
-        double GigaByte=textoScanner.nextDouble();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("escriba cuantos GB tiene su disco duro");
+            double GigaByte=textoScanner.nextDouble();
         
-        double MegaByte = GigaByte*1024;
-        double KiloByte = MegaByte*1024;
-        double Byte = KiloByte*1024;
+            double MegaByte = GigaByte*1024;
+            double KiloByte = MegaByte*1024;
+            double Byte = KiloByte*1024;
         
-        System.out.println("en megabytes"+MegaByte);
-        System.out.println("en kilobytes"+KiloByte);
-        System.out.println("en bytes"+Byte);
+            System.out.println("en megabytes"+MegaByte);
+            System.out.println("en kilobytes"+KiloByte);
+            System.out.println("en bytes"+Byte);
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
 }
