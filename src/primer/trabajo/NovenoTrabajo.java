c4eea2fb49e0526be6e611247b5222e1793189f3
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-27 20:02:01 -0500

    Fourth commit scanner

--- src/primer/trabajo/NovenoTrabajo.java
+++ src/primer/trabajo/NovenoTrabajo.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 public class NovenoTrabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        int contenedor=0;
-        for(int i=1;i<4;i++){
-            System.out.println("ingresa el valor de la venta");
-            int venta=textoScanner.nextInt();
-            contenedor=contenedor+venta;
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            int contenedor=0;
+            for(int i=1;i<4;i++){
+                System.out.println("ingresa el valor de la venta");
+                int venta=textoScanner.nextInt();
+                contenedor=contenedor+venta;
+            }
+        
+            double porcentaje=contenedor*0.12;
+            double sueldo=porcentaje+680000;
+        
+            System.out.println("el sueldo del mes fue de: "+sueldo);
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
-        
-        double porcentaje=contenedor*0.12;
-        double sueldo=porcentaje+680000;
-        
-        System.out.println("el sueldo del mes fue de: "+sueldo);
     }
     
 }
