7c7556d79ca26e097891b15517692ac4f590e8ac
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-28 23:00:36 -0500

    fourths commit scanner

--- src/primer/trabajo/DecimoTrabajo.java
+++ src/primer/trabajo/DecimoTrabajo.java
@@ -5,25 +5,29 @@ import java.util.Scanner;
 public class DecimoTrabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("cuantas horas de trabajo son");
-        int Horas=textoScanner.nextInt();
-        System.out.println("cunatos dias trabajo");
-        int Dias=textoScanner.nextInt();
-        System.out.println("cuants horas extras trabajo");
-        int HExtra=textoScanner.nextInt();
-        System.out.println("cuantos hijos tiene");
-        int Hijo=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)){
+            
+            System.out.println("cuantas horas de trabajo son");
+            int Horas=textoScanner.nextInt();
+            System.out.println("cunatos dias trabajo");
+            int Dias=textoScanner.nextInt();
+            System.out.println("cuants horas extras trabajo");
+            int HExtra=textoScanner.nextInt();
+            System.out.println("cuantos hijos tiene");
+            int Hijo=textoScanner.nextInt();
         
-        int extra=HExtra*12000;
-        int hijo= Hijo*25000;
+            int extra=HExtra*12000;
+            int hijo= Hijo*25000;
         
-        int dia=Horas*10000;
-        int mes=dia*Dias;
-        int sueldo=mes+extra+hijo;
+            int dia=Horas*10000;
+            int mes=dia*Dias;
+            int sueldo=mes+extra+hijo;
         
-        System.out.println("el sueldo que tiene es de "+sueldo);
+            System.out.println("el sueldo que tiene es de "+sueldo);            
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
 }
