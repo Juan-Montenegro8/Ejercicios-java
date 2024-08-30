ef9cc487602dbe5c93f71dd4977d9b79d9ec9b2f
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-29 21:09:30 -0500

    fifth commit scanner

--- src/primer/trabajo/OnceavoTrabajo.java
+++ src/primer/trabajo/OnceavoTrabajo.java
@@ -5,24 +5,29 @@ import java.util.Scanner;
 public class OnceavoTrabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese el numero");
-        int numero1=textoScanner.nextInt();
-        System.out.println("el otro");
-        int numero2=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            
+            System.out.println("ingrese el numero");
+            int numero1=textoScanner.nextInt();
+            System.out.println("el otro");
+            int numero2=textoScanner.nextInt();
         
-        int respuesta1=numero1%10;
-        int respuesta2=numero2%10;
+            int respuesta1=numero1%10;
+            int respuesta2=numero2%10;
         
-        int respuesta3=numero1-respuesta1;
-        int respuesta4=numero2-respuesta2;
+            int respuesta3=numero1-respuesta1;
+            int respuesta4=numero2-respuesta2;
         
-        int respuesta5=respuesta3+respuesta2;
-        int respuesta6=respuesta4+respuesta1;
+            int respuesta5=respuesta3+respuesta2;
+            int respuesta6=respuesta4+respuesta1;
         
-        System.out.println(""+respuesta5);
-        System.out.println(""+respuesta6);
+            System.out.println(""+respuesta5);
+            System.out.println(""+respuesta6);
+
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
 }
