5303b1ee0012b195fba2afb7789622bd218cb395
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-29 21:10:38 -0500

    fifths commit scanner

--- src/primer/trabajo/DoceavoTrabajo.java
+++ src/primer/trabajo/DoceavoTrabajo.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 public class DoceavoTrabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("escriba la cantidad de segundos");
-        int segundos=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)){
+            
+            System.out.println("escriba la cantidad de segundos");
+            int segundos=textoScanner.nextInt();
         
-        int horas=segundos/3600;
-        int hora=segundos%3600;
-        int minutos=hora/60;
-        int segundo=hora%60;
+            int horas=segundos/3600;
+            int hora=segundos%3600;
+            int minutos=hora/60;
+            int segundo=hora%60;
         
-        System.out.println(horas+" hora "+minutos+" minutos "+segundo+" segundos");
+            System.out.println(horas+" hora "+minutos+" minutos "+segundo+" segundos");
+
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
 }
