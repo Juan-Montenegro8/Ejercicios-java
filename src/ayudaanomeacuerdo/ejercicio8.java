14e63f84c726aa6b38d8f78fcae048948a1eefc7
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-30 20:36:33 -0500

    Fifths commit scanner

--- src/ayudaanomeacuerdo/ejercicio8.java
+++ src/ayudaanomeacuerdo/ejercicio8.java
@@ -4,25 +4,28 @@ public class ejercicio8 {
 
     public static void main(String[] args) {
         Random aleatorio=new Random();
-        Scanner textoScanner= new Scanner(System.in);
-        int vector []=new int[100];
-        for (int i = 0; i < 100; i++) {
-            vector[i]=aleatorio.nextInt(100);
-        }
-        System.out.println("los valores ramdon fueros:");
-        for (int i = 0; i < 90; i++) {
-            System.out.print(vector[i]+" ");
-        }
-        System.out.println();
-        System.out.println("escriba el numero que quiere encontrar:");
-        int numero =textoScanner.nextInt();
-        int contador=0; 
-        for (int i = 0; i < 100; i++) {
-            if(vector[i]==numero){
-                contador++;
+        try (Scanner textoScanner= new Scanner(System.in)){
+            int vector []=new int[100];
+            for (int i = 0; i < 100; i++) {
+                vector[i]=aleatorio.nextInt(100);
             }
+            System.out.println("los valores ramdon fueros:");
+            for (int i = 0; i < 90; i++) {
+                System.out.print(vector[i]+" ");
+            }
+            System.out.println();
+            System.out.println("escriba el numero que quiere encontrar:");
+            int numero =textoScanner.nextInt();
+            int contador=0; 
+            for (int i = 0; i < 100; i++) {
+                if(vector[i]==numero){
+                    contador++;
+                }
+            }
+            System.out.println("el numero de veces que se repitio el numero "+numero+" es: "+contador);
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
-        System.out.println("el numero de veces que se repitio el numero "+numero+" es: "+contador);
     }
     
 }
